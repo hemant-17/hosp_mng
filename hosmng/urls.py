822016cca5f1d83c69d2270ae857929f20a43613
Hemant <yadavhemant1716@gmail.com>
2020-05-26 12:19:40 +0530

    Password reset added

--- hosmng/urls.py
+++ hosmng/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.index,name='index'),
@@ -35,4 +36,23 @@ urlpatterns = [
     path('delete_doc/<str:id>',views.delete_doc,name='delete_doc'),
     path('hospital_acc/<str:id>',views.hospital_acc,name='hospital_acc'),
     path('send/<str:id>',views.send,name='send'),
+
+    ## Password reset url's
+
+    path('reset_password/',
+     auth_views.PasswordResetView.as_view(template_name="password_reset.html"),
+     name="reset_password"),
+
+    path('reset_password_sent/',
+        auth_views.PasswordResetDoneView.as_view(template_name="password_reset_sent.html"),
+        name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>/',
+     auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_form.html"),
+     name="password_reset_confirm"),
+
+    path('reset_password_complete/',
+        auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_done.html"),
+        name="password_reset_complete"),
+
 ]
